bf1e6836761b66d922359ab016b67945f484215d
Aaron Hunter <aaron@aaron-debian.localdomain>
2012-02-12 01:05:06 -0800

    Modified migration name to be consistent with rails and work with rake:db

--- lib/generators/elan_parser/install/install_generator.rb
+++ lib/generators/elan_parser/install/install_generator.rb
@@ -4,7 +4,7 @@ module ElanParser
  module Generators
   class InstallGenerator < ::Rails::Generators::Base
     include Rails::Generators::Migration
-    source_root File.expand_path('../templates', __FILE__)
+    source_root File.expand_path('../../templates', __FILE__)
     desc "Add the migrations"
 
       def self.next_migration_number(path)
