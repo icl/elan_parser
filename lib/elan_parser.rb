651156f84bf1386092e1b49f4c7ec45fed0711c7
Aaron Hunter <aaron@aaron-debian.localdomain>
2012-02-13 14:35:16 -0800

    Added Destroyer helper.

--- lib/elan_parser.rb
+++
@@ -1,9 +0,0 @@
-require 'elan_parser/version'
-
-module ElanParser
-  autoload :save_xml 'elan_parser_save_xml'
-  autoload :db_model 'elan_parser_db_model'
-  autoload :build_xml 'elan_parser_build_xml'
-  autoload :happymapper 'elan_parser_happymapper'
-  autoload :helper 'elan_parser_helper'
-end
--- lib/elan_parser_helper.rb
+++ lib/elan_parser_helper.rb
@@ -1,37 +1,39 @@
 module ElanParser
   module Helper
-    def destroy_children(annotation_document)
-      annotation_document.header.media_descriptors.each do |md|
-        md.destroy
-      end
-
-      annotation_document.header.properties.each do |p|
-        p.destroy
-      end
-
-      annotation_document.time_order.time_slots.each do |ts|
-        ts.destroy
-      end
-
-      annotation_document.tiers.each do |t|
-        t.annotations.each do |a|
-          a.alignable_annotation.destroy
-          a.destroy
+    class Destroyer
+      def destroy_children(annotation_document)
+        annotation_document.header.media_descriptors.each do |md|
+          md.destroy
         end
 
-        t.destroy
-      end
+        annotation_document.header.properties.each do |p|
+          p.destroy
+        end
 
-      annotation_document.linguistic_types.each do |l|
-        l.destroy
-      end
+        annotation_document.time_order.time_slots.each do |ts|
+          ts.destroy
+        end
 
-      annotation_document.constraints.each do |c|
-        c.destroy
-      end
+        annotation_document.tiers.each do |t|
+          t.annotations.each do |a|
+            a.alignable_annotation.destroy
+            a.destroy
+          end
 
-      annotation_document.locales.each do |l|
-        l.destroy
+          t.destroy
+        end
+
+        annotation_document.linguistic_types.each do |l|
+          l.destroy
+        end
+
+        annotation_document.constraints.each do |c|
+          c.destroy
+        end
+
+        annotation_document.locales.each do |l|
+          l.destroy
+        end
       end
     end
   end
