d87c569d75b4b8c09faf68317d4ce6328c237d3e
Aaron Hunter <aaron@aaron-debian.localdomain>
2012-02-13 14:29:35 -0800

    Added autoloader

--- lib/elan_parser.rb
+++ lib/elan_parser.rb
@@ -1,5 +1,9 @@
-require 'elan_parser_save_xml'
-require 'elan_parser_db_model'
-require 'elan_parser_build_xml'
-require 'elan_parser_happymapper'
-require 'elan_parser_helper'
+require 'elan_parser/version'
+
+module ElanParser
+  autoload :save_xml 'elan_parser_save_xml'
+  autoload :db_model 'elan_parser_db_model'
+  autoload :build_xml 'elan_parser_build_xml'
+  autoload :happymapper 'elan_parser_happymapper'
+  autoload :helper 'elan_parser_helper'
+end
